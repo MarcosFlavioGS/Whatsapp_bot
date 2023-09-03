c502e1827568a01943384b366b4916b572671e73
Marcos Flavio <mfghost69@gmail.com>
2023-09-03 13:24:48 -0300

    Feat :Add way to continue from last contact.

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
     driver = webdriver.Chrome()  # Or webdriver.Firefox() for Firefox
     run = Run()
     json_data = get_config(run)
+    found_last = False
 
     initialization_msg()
     time.sleep(5)
@@ -27,20 +28,23 @@ if __name__ == "__main__":
     time.sleep(20)
 
     for contact in run.contacts:
-        try:
-            message = re.sub(r'PESSOA', contact, str(run.message))
-            wp.find_chat(driver, contact)
-            time.sleep(1)
-            wp.send_message(driver, message)
-            for line in run.site:
-                wp.send_link(driver, line, run.sleep_link)
-        except Exception:
-            print(
-                f"Houve um problema com o contato: {contact}."
-            )
-        run.last_contact = contact
-        update_json(json_data, "ultimo contato", contact)
-        time.sleep(run.sleep_contact)
+        if found_last or run.restart:
+            try:
+                message = re.sub(r'PESSOA', contact, str(run.message))
+                wp.find_chat(driver, contact)
+                time.sleep(1)
+                wp.send_message(driver, message)
+                for line in run.site:
+                    wp.send_link(driver, line, run.sleep_link)
+            except Exception:
+                print(
+                    f"Houve um problema com o contato: {contact}."
+                )
+                run.last_contact = contact
+                update_json(json_data, "ultimo contato", contact)
+                time.sleep(run.sleep_contact)
+        elif contact == run.last_contact:
+            found_last = True
 
     wp.close_browser(driver)
     input(
